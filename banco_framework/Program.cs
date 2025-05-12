e291b8b60cf0e9d281a67da32ebb481fb534606d
ValadaresA-Frwk <adrianovaladares@frwk.com.br>
2025-05-12 15:09:10 -0300

    Feat: Implemented Chalange 1.1

--- banco_framework/Program.cs
+++ banco_framework/Program.cs
@@ -1,7 +1,17 @@
 ﻿using Domain.Model;
 
+namespace bancoFramework;
+
 internal class Program
 {
+
+    private static readonly Dictionary<short, string> menuOptions = new()
+        {
+            {1,  "Depósito"},
+            {2,  "Saque"},
+            {3,  "Sair"}
+        };
+
     private static void Main(string[] args)
     {
         Console.Clear();
@@ -9,6 +19,7 @@ internal class Program
         Console.WriteLine("Por favor, identifique-se");
         Console.WriteLine("");
         var pessoa = Identificacao();
+        BeginOperation(pessoa);
     }
 
     static Pessoa Identificacao()
@@ -25,9 +36,61 @@ internal class Program
         pessoa.Cpf = Console.ReadLine();
         Console.Clear();
 
-        Console.WriteLine($"Como posso ajudar {pessoa.Nome}?");
-        Console.ReadKey();
-        
+        // Console.WriteLine($"Como posso ajudar {pessoa.Nome}?");
+        // Console.ReadKey();
+
         return pessoa;
+    }
+
+    private static void BeginOperation(Pessoa pessoa)
+    {
+        Console.WriteLine($"Como posso ajudar {pessoa.Nome}?");
+        ShowMenuOptions();
+        OnSelecOption(pessoa);
+    }
+    private static void ShowMenuOptions()
+    {
+        foreach (KeyValuePair<short, string> option in menuOptions)
+        {
+            Console.WriteLine($"{option.Key} - {option.Value}");
+        }
+        Console.WriteLine("-------------");
+        Console.WriteLine("Selecione uma opção:");
+    }
+
+    private static void OnSelecOption(Pessoa pessoa)
+    {
+        try
+        {
+            short option = Convert.ToInt16(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.Clear();
+                    Console.WriteLine($"{menuOptions[1]}");
+                    Console.ReadKey();
+                    break;
+
+                case 2:
+                    Console.Clear();
+                    Console.WriteLine($"{menuOptions[2]}");
+                    Console.ReadKey();
+                    break;
+
+                case 3:
+                    break;
+
+                default:
+                    Console.Clear();
+                    BeginOperation(pessoa);
+                    break;
+            }
+        }
+        catch (System.Exception)
+        {
+
+            Console.Clear();
+            BeginOperation(pessoa);
+        }
     }
 }
