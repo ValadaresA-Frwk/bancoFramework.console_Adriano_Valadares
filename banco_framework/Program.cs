a4405256733c808075a3f5836162f8e03d65196f
ValadaresA-Frwk <adrianovaladares@frwk.com.br>
2025-05-14 11:09:59 -0300

    Feat: Implemented deposit and withdraw functionality

--- banco_framework/Program.cs
+++ banco_framework/Program.cs
@@ -1,16 +1,17 @@
-﻿using Domain.Model;
+using Application;
+using Domain.Model;
 
 namespace bancoFramework;
 
 internal class Program
 {
-
     private static readonly Dictionary<short, string> menuOptions = new()
         {
             {1,  "Depósito"},
             {2,  "Saque"},
             {3,  "Sair"}
         };
+    private static readonly Calculo CalcManager = new Calculo();
 
     private static void Main(string[] args)
     {
@@ -22,32 +23,30 @@ internal class Program
         BeginOperation(pessoa);
     }
 
-    static Pessoa Identificacao()
+    static Cliente Identificacao()
     {
-        var pessoa = new Pessoa();
+        var cliente = new Cliente();
 
         Console.WriteLine("Seu número de identificação:");
-        pessoa.Id = int.Parse(Console.ReadLine());
+        cliente.Id = int.Parse(Console.ReadLine());
 
         Console.WriteLine("Seu nome:");
-        pessoa.Nome = Console.ReadLine();
+        cliente.Nome = Console.ReadLine();
 
         Console.WriteLine("Seu CPF:");
-        pessoa.Cpf = Console.ReadLine();
+        cliente.Cpf = Console.ReadLine();
         Console.Clear();
 
-        // Console.WriteLine($"Como posso ajudar {pessoa.Nome}?");
-        // Console.ReadKey();
-
-        return pessoa;
+        return cliente;
     }
 
-    private static void BeginOperation(Pessoa pessoa)
+    private static void BeginOperation(Cliente pessoa)
     {
+        Console.WriteLine($"Seu saldo atual é: {pessoa.Saldo}");
         Console.WriteLine($"Como posso ajudar {pessoa.Nome}?");
         ShowMenuOptions();
         short option = ReadOption();
-        OnSelecOption(pessoa, option);
+        OnSelectOption(pessoa, option);
     }
     private static void ShowMenuOptions()
     {
@@ -65,26 +64,26 @@ internal class Program
         {
             return Convert.ToInt16(Console.ReadLine());
         }
-        catch (System.Exception)
+        catch (Exception)
         {
             return 0;
         }
     }
 
-    private static void OnSelecOption(Pessoa pessoa, short option)
+    private static void OnSelectOption(Cliente cliente, short option)
     {
         switch (option)
         {
             case 1:
                 Console.Clear();
                 Console.WriteLine($"{menuOptions[1]}");
-                Console.ReadKey();
+                DepositAction(cliente);
                 break;
 
             case 2:
                 Console.Clear();
                 Console.WriteLine($"{menuOptions[2]}");
-                Console.ReadKey();
+                WithdrawAction(cliente);
                 break;
 
             case 3:
@@ -92,8 +91,40 @@ internal class Program
 
             default:
                 Console.Clear();
-                BeginOperation(pessoa);
+                BeginOperation(cliente);
                 break;
+        }
+    }
+
+    private static void DepositAction(Cliente cliente)
+    {
+        try
+        {
+            Console.WriteLine("Digite o valor:");
+            double value = Convert.ToDouble(Console.ReadLine());
+            cliente.Saldo = CalcManager.Soma((float)value, cliente.Saldo);
+            Console.Clear();
+            BeginOperation(cliente);
+        }
+        catch (Exception)
+        {
+            DepositAction(cliente);
+        }
+    }
+
+    private static void WithdrawAction(Cliente cliente)
+    {
+        try
+        {
+            Console.WriteLine("Digite o valor:");
+            double value = Convert.ToDouble(Console.ReadLine());
+            cliente.Saldo = CalcManager.Subtrair(n2: (float)value, n1: cliente.Saldo);
+            Console.Clear();
+            BeginOperation(cliente);
+        }
+        catch (Exception)
+        {
+            WithdrawAction(cliente);
         }
     }
 }
